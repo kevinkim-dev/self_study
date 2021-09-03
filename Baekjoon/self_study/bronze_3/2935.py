d95b3e8ebe35da29d8e334c749971e66072f6504
kevinkim <shkim2000@gmail.com>
2021-09-03 22:52:23 +0900

    0903 algorithm study

---
+++ Baekjoon/self_study/bronze_3/2935.py
@@ -0,0 +1,12 @@
+###########################
+#  BaekJoon 2935번
+#  by 김승현                
+###########################
+
+# Q. 소음 
+
+a = int(input())
+if input() == '+':
+    print(a + int(input()))
+else:
+    print(a*int(input()))

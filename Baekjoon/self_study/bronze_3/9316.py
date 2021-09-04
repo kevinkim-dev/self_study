6fb095de066f5b10d6526cc7314dfa0b55f2b5b7
kevinkim <shkim2000@gmail.com>
2021-09-04 23:42:33 +0900

    0904 algorithm study

---
+++ Baekjoon/self_study/bronze_3/9316.py
@@ -0,0 +1,9 @@
+###########################
+#  BaekJoon 9316번
+#  by 김승현                
+###########################
+
+# Q. Hello Judge
+
+for i in range(1, int(input())+1):
+    print('Hello World, Judge ' + str(i) +'!')

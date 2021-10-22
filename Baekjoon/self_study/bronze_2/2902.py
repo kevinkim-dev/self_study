565eb3a95ae1e20a3f21f3a8126a135fbd7d25ab
kevinkim <shkim2000@gmail.com>
2021-10-22 23:52:41 +0900

    1022 algorithm study

---
+++ Baekjoon/self_study/bronze_2/2902.py
@@ -0,0 +1,19 @@
+###########################
+#  BaekJoon 2902번
+#  by 김승현                
+###########################
+
+# Q. KMP는 왜 KMP일까?
+
+name = input()
+
+ans = ''
+is_first = True
+for s in name:
+    if is_first:
+        ans += s
+        is_first = False
+    elif s == '-':
+        is_first = True
+
+print(ans)

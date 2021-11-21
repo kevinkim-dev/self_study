850c309bfe80e70e5a72bf653d8685a858fcb8db
kevinkim <shkim2000@gmail.com>
2021-11-21 23:57:39 +0900

    1121 algorithm study

---
+++ Baekjoon/self_study/bronze_4/5717.py
@@ -0,0 +1,12 @@
+###########################
+#  BaekJoon 5717번
+#  by 김승현                
+###########################
+
+# Q. 상근이의 친구들
+
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break
+    print(a+b)
